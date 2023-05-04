dee56b63026446a34aaa4abb8647c1556074190a
UnDeFined <80025598+coderWuzh@users.noreply.github.com>
2023-05-04 21:34:39 +0800

    更改目录

--- serve/db/sql.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'wzh112233',
-  database: 'my_tea_db'
-})
-module.exports = connection
